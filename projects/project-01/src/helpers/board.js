e8be92c2f61872cd4dc67d538d096fe96e33ed39
MPardal <miguelpardal.esparis@ext.essilor.com>
2025-04-23 09:12:03 +0200

    tictactoe

---
+++ projects/project-01/src/helpers/board.js
@@ -0,0 +1,19 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo;
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a];
+        }
+    };
+    return null;
+};
+
+export const checkEndGame = (board) => {
+    return board.every((sqr) => sqr !== null);
+};
